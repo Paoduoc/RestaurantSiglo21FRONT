7a32ca41a1361ca91f47d049eb9e345fe83f956c
Ivette <italita.noe@gmail.com>
2022-10-04 15:46:30 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState("")
+  const [totalUsers, setTotalUsers] = useState(0)
+  const [password, setPassword] = useState("")
+
+  const handleSetUsername = (event) => {
+    setUsername(event.target.value)
+  }
+  const handleSetPassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/users")
+      .then(response => response.json())
+      .then(response => {
+        setTotalUsers(response.msg.length)
+      })
+  })
+  
+  const login = () => {
+    fetch("", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })  
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Total usuarios {totalUsers}</label>
+        <label>Username ({})</label>
+        <input type="text" value={username} onChange={handleSetUsername} />
+
+        <label>Password ({password})</label>
+        <input type="password" value={password} onChange={handleSetPassword} />
       </header>
     </div>
   );
