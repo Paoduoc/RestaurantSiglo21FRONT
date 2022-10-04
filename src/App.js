1c482c39113639e6b7ddb6ff35738182bc0253a4
Ivette <italita.noe@gmail.com>
2022-10-04 18:07:44 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [username, setUsername] = useState("")
-  const [totalUsers, setTotalUsers] = useState(0)
   const [password, setPassword] = useState("")
 
   const handleSetUsername = (event) => {
@@ -14,35 +12,53 @@ function App() {
     setPassword(event.target.value)
   }
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/v1/users")
-      .then(response => response.json())
-      .then(response => {
-        setTotalUsers(response.msg.length)
-      })
-  })
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/api/v1/users")
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       setTotalUsers(response.msg.length)
+  //     })
+  // })
   
   const login = () => {
-    fetch("", {
+    fetch("http://192.168.1.3:8080/api/v1/auth/login", {
       method: 'POST',
       headers: {
+        'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-    })  
+      body: JSON.stringify({
+        correo: username,
+        contrasenna: password
+      })
+    }).then(response => response.json())
+      .then(response => {
+        console.log(response)
+      })
   }
 
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <label>Total usuarios {totalUsers}</label>
-        <label>Username ({})</label>
-        <input type="text" value={username} onChange={handleSetUsername} />
+      <div className='App-header' >
+        <div className='row'>
+          <div className='col col-4'>
+          </div>
+          <div className='col col-4'>
+            <h1>Iniciar sesión</h1>
+            <label className='form-label'>Correo</label>
+            <input className='form-control' type="text" value={username} onChange={handleSetUsername} />
 
-        <label>Password ({password})</label>
-        <input type="password" value={password} onChange={handleSetPassword} />
-      </header>
+            <label className='form-label'>Contraseña</label>
+            <input className='form-control' type="password" value={password} onChange={handleSetPassword} />
+
+            <button className='btn btn-success mt-3' onClick={() => login()}>
+              Acceder
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
